9dabdc86b9c8525fb3958cd049686db27ec8be6a
kacperpucylo <pucylek@wp.pl>
2023-09-30 22:33:30 +0200

    working version (no sound though)

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -2,6 +2,7 @@ extern crate sdl2;
 
 use crisp8_core::*;
 use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
@@ -13,6 +14,7 @@ use std::io::Read;
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
 
 fn main()
 {
@@ -56,12 +58,28 @@ fn main()
 		{
 			match event
 			{
-				Event::Quit { .. } => break 'main,
+				Event::Quit { .. } | Event::KeyDown {keycode: Some(Keycode::Escape), .. } => break 'main,
+				Event::KeyDown { keycode: Some(key), .. } => {
+					if let Some(k) = convert_key(key)
+					{
+						crisp8.keypress(k, true);
+					}
+				},
+				Event::KeyUp { keycode: Some(key), .. } => {
+					if let Some(k) = convert_key(key)
+					{
+						crisp8.keypress(k, false);
+					}
+				}
 				_ => {}
 			}
 		}
 
-		crisp8.tick();
+		for _ in 0..TICKS_PER_FRAME
+		{
+			crisp8.tick();
+		}
+		crisp8.tick_timers();
 		draw_screen(&crisp8, &mut canvas);
 	}
 }
@@ -86,4 +104,28 @@ fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>)
 		}
 	}
 	canvas.present();
+}
+
+fn convert_key(key: Keycode) -> Option<usize>
+{
+	match key
+	{
+		Keycode::Num1 => Some(0x1),
+		Keycode::Num2 => Some(0x2),
+		Keycode::Num3 => Some(0x3),
+		Keycode::Num4 => Some(0xc),
+		Keycode::Q => Some(0x4),
+		Keycode::W => Some(0x5),
+		Keycode::E => Some(0x6),
+		Keycode::R => Some(0xd),
+		Keycode::A => Some(0x7),
+		Keycode::S => Some(0x8),
+		Keycode::D => Some(0x9),
+		Keycode::F => Some(0xe),
+		Keycode::Z => Some(0xa),
+		Keycode::X => Some(0x0),
+		Keycode::C => Some(0xb),
+		Keycode::V => Some(0xf),
+		_ => None
+	}
 }
